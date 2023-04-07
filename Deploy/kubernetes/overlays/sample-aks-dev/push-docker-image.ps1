9c3f576cb0de2171b048680fe6248c6326a22a64
Joseph Tam <josephtam@netvigator.com>
2023-04-07 18:43:14 +0800

    Update kubernetes manifests

---
+++ Deploy/kubernetes/overlays/sample-aks-dev/push-docker-image.ps1
@@ -0,0 +1,26 @@
+$tag = "1"
+
+$registry = "vspsample.azurecr.io"
+
+$images = @(
+  "api-gateway",
+  "community-api",
+  "history-api",
+  "identity-provider",
+  "library-api",
+  "search-api",
+  "storage-api",
+  "subscriptions-api",
+  "users-api",
+  "video-manager-api",
+  "video-manager-signalrhub",
+  "video-processor-application",
+  "video-store-api",
+  "web-status"
+)
+
+foreach ($image in $images) {
+  $tagged_image = "${registry}/${image}:${tag}"
+  docker tag $image $tagged_image
+  docker push $tagged_image
+}
