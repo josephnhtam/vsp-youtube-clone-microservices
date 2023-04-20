27c9e71979c766d2f7009a07ebde2970bc62f27d
Joseph Tam <josephtam@netvigator.com>
2023-04-21 06:42:27 +0800

    Revert

--- Backend/BuildingBlocks/Infrastructure.EFCore/ResilientTransactionExtensions.cs
+++ Backend/BuildingBlocks/Infrastructure.EFCore/ResilientTransactionExtensions.cs
@@ -8,10 +8,6 @@ namespace Infrastructure.EFCore {
             var strategy = dbContext.Database.CreateExecutionStrategy();
 
             await strategy.ExecuteAsync(async () => {
-                foreach (var entry in dbContext.ChangeTracker.Entries()) {
-                    entry.State = EntityState.Detached;
-                }
-
                 using var transaction = await (isolationLevel.HasValue ?
                         dbContext.Database.BeginTransactionAsync(isolationLevel.Value, cancellationToken) :
                         dbContext.Database.BeginTransactionAsync(cancellationToken));
